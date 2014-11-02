2c7e8955566cefa3ae9711cd2638000a15ce447a
hattmarris <mattharris89@gmail.com>
2014-11-01 22:53:44 -0700

    Dynamically added Cores as radio options

--- functions/export/export-projects-admin.php
+++ functions/export/export-projects-admin.php
@@ -6,8 +6,29 @@
 $html = '<div class="wrap"><div id="icon-tools" class="icon32"></div>
 			<h2>Export as Table 5 Report to CSV or PDF</h2>
 		</div>';
-$html .= '<form action="" method="post" id="export-pdf">
-<p>
+$html .= '<form action="" method="post" id="export-pdf">';
+$html .= '<h3>Core:</h3>';
+$html .= '<p><ul>';
+
+/**
+* Dynamically add Cores as radio options
+*/
+
+$cores = get_terms( 'core', 'hide_empty=0' );
+foreach($cores as $core){
+	$html .= '<li><label>
+	<input type="radio" name="core" value="'.$core->slug.'">'.$core->name
+	.'</label></li>';
+}
+$html .= '</ul></p>';
+
+/**
+* Adds radio selection of file format
+*/
+
+$html .= '<h3>Format:</h3>';
+$html .=
+'<p>
 	<label>
 		<input type="radio" name="type" value="csv"> CSV
 	</label>
