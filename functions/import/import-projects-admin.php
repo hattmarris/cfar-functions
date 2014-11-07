7cd2791cf4d12330cd1fdb3c15049fb169a9abda
hattmarris <mattharris89@gmail.com>
2014-11-07 17:36:06 -0500

    removed import posts as drafts from impor projects admin page

--- functions/import/import-projects-admin.php
+++ functions/import/import-projects-admin.php
@@ -4,13 +4,13 @@
 <div class="wrap">
     <h2>Import CSV</h2>
     <form class="add:the-list: validate" method="post" enctype="multipart/form-data">
-        <!-- Import as draft -->
+        <!-- Import as draft 
         <p>
         <input name="_csv_importer_import_as_draft" type="hidden" value="publish" />
         <label><input name="csv_importer_import_as_draft" type="checkbox" <?php if ('draft' == $opt_draft) { echo 'checked="checked"'; } ?> value="draft" /> Import posts as drafts</label>
-        </p>
+        </p>-->
 
-        <!-- Parent category -->
+        <!-- Parent core -->
         <p>Core: <?php wp_dropdown_categories(array('show_option_all' => 'Select one ...', 'taxonomy' => 'core', 'hide_empty' => 0, 'hierarchical' => 1, 'show_count' => 0, 'name' => 'csv_importer_core', 'orderby' => 'name', 'selected' => $opt_cat));?><br/>
             <!--<small>This will create new categories inside the category parent you choose.</small>--></p>
 
